d1c8ea5d495611e1852d27ac70851f8f4cd3da73
Geoveza <geoveza21@gmail.com>
2024-03-24 17:00:26 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-d7498837-416c-4085-8856-44f29e4fe03d/)
 
 
 echo "$curl_output" >> Update.tsx
